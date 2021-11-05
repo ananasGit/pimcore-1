56cfa56f79b019264e61a4815235433600e8430e
marko-todic <marko.todic@htecgroup.com>
2021-11-05 11:23:58 +0100

    Add type and dir scope options to search-backend-reindex command

--- bundles/CoreBundle/Command/SearchBackendReindexCommand.php
+++ bundles/CoreBundle/Command/SearchBackendReindexCommand.php
@@ -22,29 +22,50 @@ use Pimcore\Model\DataObject;
 use Pimcore\Model\Element\Service;
 use Pimcore\Model\Search;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class SearchBackendReindexCommand extends AbstractCommand
 {
+
+    protected $db;
+
+    protected $type;
+
+    protected $types;
+
+    protected $dir;
+
     protected function configure()
     {
         $this
             ->setName('pimcore:search-backend-reindex')
             ->setAliases(['search-backend-reindex'])
-            ->setDescription('Re-indexes the backend search of pimcore');
+            ->setDescription('Re-indexes the backend search of pimcore')
+            ->addOption('type', 't', InputOption::VALUE_OPTIONAL, 'Type of data to index: asset, document or object')
+            ->addOption('dir', 'd', InputOption::VALUE_OPTIONAL, 'Directory within data is to be indexed');
     }
 
     /**
      * @inheritDoc
+     * @throws \Exception
      */
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        // clear all data
-        $db = \Pimcore\Db::get();
-        $db->query('TRUNCATE `search_backend_data`;');
+        $this->db = \Pimcore\Db::get();
+        $this->type = trim($input->getOption('type'));
+        $this->dir = trim($input->getOption('dir'));
 
+        $allSupportedTypes = ['asset', 'document', 'object'];
+        if ($this->type && !in_array($this->type, $allSupportedTypes)) {
+            throw new \Exception('Invalid type "'.$this->type.'". Supported types are: ' . implode(', ', $allSupportedTypes));
+        }
+        $types = $this->type ? [$this->type] : ['asset', 'document', 'object'];
+
+        $this->clearIndex();
+
+        $startTime = time();
         $elementsPerLoop = 100;
-        $types = ['asset', 'document', 'object'];
 
         foreach ($types as $type) {
             $baseClass = Service::getBaseClassNameForElement($type);
@@ -56,9 +77,9 @@ class SearchBackendReindexCommand extends AbstractCommand
 
             if (method_exists($list, 'setObjectTypes')) {
                 $list->setObjectTypes([
-                    DataObject::OBJECT_TYPE_OBJECT,
-                    DataObject::OBJECT_TYPE_FOLDER,
-                    DataObject::OBJECT_TYPE_VARIANT,
+                    DataObject\AbstractObject::OBJECT_TYPE_OBJECT,
+                    DataObject\AbstractObject::OBJECT_TYPE_FOLDER,
+                    DataObject\AbstractObject::OBJECT_TYPE_VARIANT,
                 ]);
             }
 
@@ -71,11 +92,16 @@ class SearchBackendReindexCommand extends AbstractCommand
                 $this->output->writeln('Processing ' .$type . ': ' . ($list->getOffset() + $elementsPerLoop) . '/' . $elementsTotal);
 
                 $elements = $list->load();
+
                 foreach ($elements as $element) {
                     try {
                         //process page count, if not exists
                         if ($element instanceof Asset\Document && $element->getCustomSetting('document_page_count')) {
                             $element->processPageCount();
+                        }
+
+                        if ($this->dir && !str_starts_with($element->getFullPath(), $this->dir)) {
+                            continue;
                         }
 
                         $searchEntry = Search\Backend\Data::getForElement($element);
@@ -94,8 +120,38 @@ class SearchBackendReindexCommand extends AbstractCommand
             }
         }
 
-        $db->query('OPTIMIZE TABLE search_backend_data;');
+        $this->db->query('OPTIMIZE TABLE search_backend_data;');
+
+        $timeSpent = round(time() - $startTime);
+        $humanReadableTimeSpent = sprintf('%02d:%02d:%02d', ($timeSpent/3600),($timeSpent/60%60), $timeSpent%60);
+
+        echo PHP_EOL;
+        $output->writeln(" <bg=green;options=bold> Done in $humanReadableTimeSpent </>");
+        echo PHP_EOL;
 
         return 0;
+    }
+
+    private function clearIndex(): void
+    {
+        if (!$this->type && !$this->dir) {
+            $this->db->query('TRUNCATE `search_backend_data`;');
+            return;
+        }
+
+        $where = [];
+
+        if ($this->type) {
+            $where[] = "`type` = '{$this->type}'";
+        }
+
+        if ($this->dir) {
+            $where[] = "`fullpath` LIKE '{$this->dir}%'";
+        }
+
+        $stringWhere = implode(' AND ', $where);
+        $query = "DELETE FROM `search_backend_data` WHERE $stringWhere;";
+
+        $this->db->query($query);
     }
 }
